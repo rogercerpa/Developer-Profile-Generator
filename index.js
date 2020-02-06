605675360de0a243a0d4520d11ef83149336bffd
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-05 19:22:11 -0500

    added inquirer, coded the prompt to ask the initial questions for the project

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const questions = [
-  
-];
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+	.prompt([
+		{
+			type    : 'input',
+			name    : 'username',
+			message : 'What is your GitHub account username?'
+		},
+		{
+			type    : 'list',
+			name    : 'color',
+			message : 'Choose one of the following colors:',
+			choices : [ 'blue', 'green', 'pink', 'red' ]
+		}
+	])
+	.then((userdata) => {
+		const username = userdata.username;
+		const usercolor = userdata.color;
+		fs.writeFile(username, JSON.stringify(userdata, null, '\t'), function(err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log('success!');
+		});
+	});
 
-function writeToFile(fileName, data) {
- 
-}
+// const questions = [
 
-function init() {
+// ];
 
-init();
+// function writeToFile(fileName, data) {
+
+// }
+
+// function init() {
+
+// init();
