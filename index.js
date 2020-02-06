effce8a8cf2bb052feaa3a32b21b6ec9677be331
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-05 20:59:36 -0500

    tested code with axios to gather information from github api, in progress

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const inquirer = require('inquirer');
 const fs = require('fs');
 inquirer
@@ -5,7 +6,7 @@ inquirer
 		{
 			type    : 'input',
 			name    : 'username',
-			message : 'What is your GitHub account username?'
+			message : 'What is your GitHub account userinput?'
 		},
 		{
 			type    : 'list',
@@ -14,14 +15,25 @@ inquirer
 			choices : [ 'blue', 'green', 'pink', 'red' ]
 		}
 	])
-	.then((userdata) => {
-		const username =
-			userdata.username.toLowerCase().split('').join('') + '.json';
-		fs.writeFile(username, JSON.stringify(userdata, null, '\t'), function(err) {
-			if (err) {
-				return console.log(err);
-			}
-			console.log('success!');
+	.then(({ username }) => {
+		// const userinput =userdata.userinput.toLowerCase().split('').join('') + '.json';
+		const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+		// fs.writeFile(userinput, JSON.stringify(userdata, null, '\t'), function(
+		// 	err
+		// ) {
+		// 	if (err) {
+		// 		return console.log(err);
+		// 	}
+		// 	console.log('success!');
+		// 	axios.get(queryUrl).then(function(res) {
+		// 		console.log(res.data);
+		// 	});
+		// });
+		axios.get(queryUrl).then(function(res) {
+			const repoInfo = res.data.map(function(repo) {
+				return repo.name;
+			});
+			console.log(repo.name);
 		});
 	});
 
